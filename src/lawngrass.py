8b1737f997977818bb923f415bf97007ce7c9aef
Степан Прокопьев <dr.aibolitarise@mail.ru>
2025-12-02 06:05:09 +0900

    # The LawnGrass class has been created.

---
+++ src/lawngrass.py
@@ -0,0 +1,34 @@
+from src.product import Product
+
+
+class LawnGrass(Product):
+    def __init__(self, name: str, description: str, price: float, quantity: int,
+                 country: str, germination_period: str, color: str):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
+    def __str__(self) -> str:
+        return (f"{self.name}, {self.color}, произв.: {self.country}, "
+                f"прорастание: {self.germination_period}, "
+                f"{int(self.price)} руб. Остаток: {self.quantity} шт.")
+
+    def __add__(self, other):
+        """Сложение двух газонных трав. Используем type() для строгой проверки класса"""
+        if type(other) is not type(self):  # Строгая проверка: тот же класс
+            raise TypeError("Можно складывать только объекты одного класса")
+
+        total_quantity = self.quantity + other.quantity
+        total_value = self.price * self.quantity + other.price * other.quantity
+        avg_price = total_value / total_quantity
+
+        return LawnGrass(
+            name=f"{self.name} & {other.name}",
+            description=self.description,
+            price=avg_price,
+            quantity=total_quantity,
+            country=f"{self.country}/{other.country}",
+            germination_period="avg",
+            color=f"{self.color}/{other.color}"
+        )
