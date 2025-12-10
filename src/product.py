24f7d035281d7bb4c19cb2463502a0ac418a9ec6
Степан Прокопьев <dr.aibolitarise@mail.ru>
2025-12-11 05:57:28 +0900

    # Modified.

--- src/product.py
+++ src/product.py
@@ -11,6 +11,10 @@ class Product(CreationLogger, BaseProduct):
     """
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
+        # Добавляем проверку на нулевое количество
+        if quantity == 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
+
         self.name = name
         self.description = description
         self.__price = price  # Приватный атрибут
